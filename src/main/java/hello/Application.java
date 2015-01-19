9a138b7b6f92462882c3f94f4d1e473625020ce8
Justin <justinuliu@gmail.com>
2015-01-19 16:15:11 +0800

    Add bootstrap.

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,17 @@
+package hello;
+
+/**
+ * Created by Justin on 2015/1/12.
+ */
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,22 @@
+package hello;
+
+/**
+ * Created by Justin on 2015/1/12.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
