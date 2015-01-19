7b01edc68712106f9e5dc8ad93f7ac430f493ea8
Justin <justinuliu@gmail.com>
2015-01-19 17:13:08 +0800

    Starting learn rest service tutorial.

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by Justin on 2015/1/19.
+ */
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
